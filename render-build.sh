04f20725c9ef678e06c259d381f86fa3b429875f
Faithfultennyson Idama <Faithfultennysonidama@gmail.com>
2025-11-19 09:07:17 +0100

    update

--- render-build.sh
+++ render-build.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
-apt-get update && apt-get install -y ffmpeg
-npm install
+set -o errexit  # Exit on first error
+set -o nounset  # Treat unset variables as errors
+set -o pipefail # Catch errors in piped commands
+
+# Install ffmpeg
+apt-get update
+apt-get install -y ffmpeg
+
+# Install Node dependencies
+npm install --production
